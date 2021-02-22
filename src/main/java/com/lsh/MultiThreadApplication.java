bfd2de7689f455fe0972445576ad9104a17d4dbd
Liu-Shihao <1692312138@qq.com>
2021-02-22 16:48:25 +0800

    创建多线程的两中方式继承Thread和实现Runnable接口

---
+++ src/main/java/com/lsh/MultiThreadApplication.java
@@ -0,0 +1,16 @@
+package com.lsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：LiuShihao
+ * @date ：Created in 2021/2/22 10:32 上午
+ * @desc ：
+ */
+@SpringBootApplication
+public class MultiThreadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MultiThreadApplication.class);
+    }
+}
