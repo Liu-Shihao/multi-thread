3e034f24f49d097df366aa408624bb9eb07ef277
LiuShihao <1692312138@qq.com>
2022-02-15 15:19:21 +0800

    retry:的Java用法

--- src/main/java/com/lsh/day01/Code03_ThreadPool.java
+++ src/main/java/com/lsh/day01/Code03_ThreadPool.java
@@ -1,5 +1,6 @@
 package com.lsh.day01;
 
+import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -19,9 +20,15 @@ public class Code03_ThreadPool {
          * 使用默认的拒绝策略是直接抛出RejectedExecutionException异常
          *
          */
+//        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 2,
+//                60L, TimeUnit.SECONDS,
+//                new LinkedBlockingQueue<>(5));
+
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 2,
                 60L, TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(5));
+                new LinkedBlockingQueue<>(5), Executors.defaultThreadFactory(), new ThreadPoolExecutor.CallerRunsPolicy());
+
+
         int cps = 1;
         int mps = 2;
         int c = 5;
@@ -36,22 +43,16 @@ public class Code03_ThreadPool {
             });
         }
         System.out.println("----------------");
-        /**
-         * 此时线程池达到线程最大值，队列已满，  再加一个线程则会直接抛出异常
-         * ----------------
-         * Exception in thread "main" java.util.concurrent.RejectedExecutionException: Task com.lsh.day01.Code03_ThreadPool$$Lambda$2/1595428806@3feba861 rejected from java.util.concurrent.ThreadPoolExecutor@5b480cf9[Running, pool size = 2, active threads = 2, queued tasks = 5, completed tasks = 0]
-         * 	at java.util.concurrent.ThreadPoolExecutor$AbortPolicy.rejectedExecution(ThreadPoolExecutor.java:2063)
-         * 	at java.util.concurrent.ThreadPoolExecutor.reject(ThreadPoolExecutor.java:830)
-         * 	at java.util.concurrent.ThreadPoolExecutor.execute(ThreadPoolExecutor.java:1379)
-         * 	at com.lsh.day01.Code03_ThreadPool.main(Code03_ThreadPool.java:38)
-         */
-        threadPoolExecutor.execute(() ->{
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        //此时线程池达到线程最大值，队列已满，  再加一个线程则会直接抛出RejectedExecutionException异常
+        for (int i = 0; i < size; i++) {
+            threadPoolExecutor.execute(() ->{
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
 
     }
 }
---
+++ src/main/java/com/lsh/day01/Code04.java
@@ -0,0 +1,46 @@
+package com.lsh.day01;
+
+/**
+ * @author ：LiuShihao
+ * @date ：Created in 2022/2/15 12:08 下午
+ * @desc ：
+ */
+public class Code04 {
+    private static final int COUNT_BITS = Integer.SIZE - 3;
+    private static final int CAPACITY   = (1 << COUNT_BITS) - 1;
+
+    private static final int RUNNING    = -1 << COUNT_BITS;
+
+    private static int workerCountOf(int c)  { return c & CAPACITY; }
+    private static int ctlOf(int rs, int wc) { return rs | wc; }
+
+    public static void main(String[] args) throws Exception {
+//        ExecutorService executorService = Executors.newFixedThreadPool(2);
+//        executorService.execute(()-> System.out.println("hello world!"));
+//        executorService.shutdown();
+//        executorService.awaitTermination(1, TimeUnit.MINUTES);
+//        print(1 << COUNT_BITS);
+//        print(CAPACITY);
+//        System.out.println(ctlOf(RUNNING, 0));
+//        print(-536870912);
+
+        retry:
+        for (;;) {
+            for (;;) {
+                System.out.println("1");
+                //break retry; 内层循环结束并且直接跳出外层循环
+                break retry;
+            }
+//            System.out.println("2");
+        }
+//        System.out.println("3");
+
+    }
+
+    public static void print(int n){
+        for (int i = 31; i >=0 ; i--) {
+            System.out.print( (n & (1 << i)) == 0 ? "0" : "1");
+        }
+        System.out.println();
+    }
+}
