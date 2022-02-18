96c571b23bd68f6cba488e2af552bb4f024a92ef
LiuShihao <1692312138@qq.com>
2022-02-18 17:03:54 +0800

    乐观锁cas；悲观锁synchronized

---
+++ src/main/java/com/lsh/day03/Code03_Atomic.java
@@ -0,0 +1,36 @@
+package com.lsh.day03;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author ：LiuShihao
+ * @date ：Created in 2022/2/18 4:27 下午
+ * @desc ：原子
+ * 使用 cas 解决多线程问题
+ *
+ */
+public class Code03_Atomic {
+
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    private static void m(){
+        atomicInteger.incrementAndGet();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(100);
+        for (int i = 0; i < 100; i++) {
+            new Thread(()->{
+                for (int j = 0; j < 10000; j++) {
+                    m();
+                }
+                latch.countDown();
+            }).start();
+        }
+        latch.await();
+
+        System.out.println(atomicInteger.get());
+
+    }
+}
--- src/main/java/com/lsh/day03/Code03_Sync2.java
+++ src/main/java/com/lsh/day03/Code03_Sync2.java
@@ -3,7 +3,12 @@ package com.lsh.day03;
 /**
  * @author ：LiuShihao
  * @date ：Created in 2022/2/18 3:59 下午
- * @desc ：
+ * @desc ：上锁的本质就是把并发的编程序列化
+ * synchronized保证 原子性、可见性，但是不保证有序性。
+ *
+ * 1. 悲观的认为这个操作会被别的线程打断（悲观锁）synchronized（上一个小程序）
+ *
+ * 2. 乐观的认为这个做不会被别的线程打断（乐观锁 自旋锁 无锁）cas操作
  */
 public class Code03_Sync2 {
     private static Object  o = new Object();
@@ -11,6 +16,11 @@ public class Code03_Sync2 {
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
             new Thread(()->{
+                /**
+                 * synchronized (o 管程){
+                 *      临界区 大 锁的粒度大；小 粒度小
+                 * }
+                 */
                 synchronized (o){
                     System.out.println(Thread.currentThread().getName()+" start!");
                     try {
