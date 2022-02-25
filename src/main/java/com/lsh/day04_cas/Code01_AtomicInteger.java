aa7165191f94cfa2e3c396fdea96dc085422149d
LiuShihao <1692312138@qq.com>
2022-02-25 10:06:39 +0800

    在多线程累加中，效率：LongAdder>AtomicLong>synchronized

--- src/main/java/com/lsh/day04_cas/Code01_AtomicInteger.java
+++ src/main/java/com/lsh/day04_cas/Code01_AtomicInteger.java
@@ -1,26 +1,50 @@
 package com.lsh.day04_cas;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * @author ：LiuShihao
  * @date ：Created in 2022/2/22 9:58 下午
- * @desc ：AtomidXXX本身方法都是原子性的，但是不能保证多个方法连续调用是原子性的。
+ * @desc ：
+ *
+ * AtomicInteger本身方法都是原子性的，底层通过cas实现 乐观锁
+ * LongAdder内部使用分段锁，所以效率高
  */
 public class Code01_AtomicInteger {
-      AtomicInteger count=  new AtomicInteger(0);
+    static Long count1 = 0L;
+    static AtomicLong count2=  new AtomicLong(0);
+    static LongAdder count3 = new LongAdder();
 
-      void m(){
-          for (int i = 0; i < 10000; i++) {
-              count.incrementAndGet();
+    static final Object lock = new Object();
+
+    static int testTime = 10000;
+
+    static int threadNum = 1000;
+
+
+    static void m1(){
+          for (int i = 0; i < testTime; i++) {
+              synchronized (lock){
+                  count1++;
+              }
           }
-      }
+    }
+    static void m2(){
+          for (int i = 0; i < testTime; i++) {
+              count2.incrementAndGet();
+          }
+    }
+    static void m3(){
+          for (int i = 0; i < testTime; i++) {
+              count3.increment();
+          }
+    }
     public static void main(String[] args) throws InterruptedException {
-        Code01_AtomicInteger code01_atomicInteger = new Code01_AtomicInteger();
-
-        Thread[] threads = new Thread[100];
-        for (int i = 0; i < 100; i++) {
-            threads[i] = new Thread(code01_atomicInteger::m,"t"+i);
+        long start1 = System.currentTimeMillis();
+        Thread[] threads = new Thread[threadNum];
+        for (int i = 0; i < threadNum; i++) {
+            threads[i] = new Thread(Code01_AtomicInteger::m1,"t"+i);
         }
         for (Thread thread : threads) {
             thread.start();
@@ -28,11 +52,34 @@ public class Code01_AtomicInteger {
         for (Thread thread : threads) {
             thread.join();
         }
-        System.out.println(code01_atomicInteger.count.get());
+        long l1 = System.currentTimeMillis() - start1;
+        System.out.println("synchronized    :"+l1+" :"+count1);
 
 
+        long start2 = System.currentTimeMillis();
+        for (int i = 0; i < threadNum; i++) {
+            threads[i] = new Thread(Code01_AtomicInteger::m2,"t"+i);
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        long l2 = System.currentTimeMillis() - start2;
+        System.out.println("AtomicLong      :"+l2+" :"+count2.get());
+
+        long start3 = System.currentTimeMillis();
+        for (int i = 0; i < threadNum; i++) {
+            threads[i] = new Thread(Code01_AtomicInteger::m3,"t"+i);
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+        long l3 = System.currentTimeMillis() - start3;
+        System.out.println("LongAdder       :"+l3+" :"+count3);
     }
-
-
-
 }
